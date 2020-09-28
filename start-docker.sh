61775d58d681c65cc358e1aba12960bd69ebc8f7
Pierre-Yves Jourel <pierre-yves.jourel@pyjou.com>
2020-09-28 14:08:28 -0400

    Remove container before start

--- start-docker.sh
+++ start-docker.sh
@@ -4,6 +4,9 @@ IMG_NAME="system-gestion-borderau"
 CTN_NAME="system-gestion-borderau-app"
 PORT_HOST="3001"
 
+docker stop $CTN_NAME
+docker rm $CTN_NAME
+
 docker build -t $IMG_NAME .
 docker run -d -p "$PORT_HOST:3001" --name $CTN_NAME $IMG_NAME
 
